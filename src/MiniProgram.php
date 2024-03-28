92b857561c1f0dc187eeaf82bd09659e913e8de0
ran <503948796@qq.com>
2024-03-28 22:54:19 +0800

    add phone auth

--- src/MiniProgram.php
+++ src/MiniProgram.php
@@ -38,6 +38,8 @@ class MiniProgram extends BaseWechat
      */
     const WXACODE_UNLIMIT_PATH = '/wxa/getwxacodeunlimit';
 
+    const WXACODE_GETUSERPHONENUMBER = '/wxa/business/getuserphonenumber';
+
     /**
      * 数据缓存前缀
      * @var string
@@ -74,7 +76,7 @@ class MiniProgram extends BaseWechat
      */
     protected function getCacheKey($name)
     {
-        return $this->cachePrefix . '_' . $this->appId . '_' . $name;
+        return $this->cachePrefix.'_'.$this->appId.'_'.$name;
     }
 
     /**
@@ -84,8 +86,9 @@ class MiniProgram extends BaseWechat
     protected function httpBuildQuery($url, array $options)
     {
         if (stripos($url, 'http://') === false && stripos($url, 'https://') === false) {
-            $url = self::WECHAT_BASE_URL . $url;
+            $url = self::WECHAT_BASE_URL.$url;
         }
+
         return parent::httpBuildQuery($url, $options);
     }
 
@@ -99,21 +102,22 @@ class MiniProgram extends BaseWechat
         if (isset($result['errcode']) && $result['errcode']) {
             $this->lastError = $result;
             Yii::warning([
-                'url' => $url,
-                'result' => $result,
-                'postOptions' => $postOptions
+                'url'         => $url,
+                'result'      => $result,
+                'postOptions' => $postOptions,
             ], __METHOD__);
             switch ($result ['errcode']) {
                 case 40001: //access_token 失效,强制更新access_token, 并更新地址重新执行请求
                     if ($force) {
-                        $url = preg_replace_callback("/access_token=([^&]*)/i", function(){
-                            return 'access_token=' . $this->getAccessToken(true);
+                        $url    = preg_replace_callback("/access_token=([^&]*)/i", function () {
+                            return 'access_token='.$this->getAccessToken(true);
                         }, $url);
                         $result = $this->parseHttpRequest($callable, $url, $postOptions, false); // 仅重新获取一次,否则容易死循环
                     }
                     break;
             }
         }
+
         return $result;
     }
 
@@ -127,10 +131,11 @@ class MiniProgram extends BaseWechat
     protected function requestAccessToken($grantType = 'client_credential')
     {
         $result = $this->httpGet(self::ACCESS_TOKEN_PATH, [
-            'appid' => $this->appId,
-            'secret' => $this->appSecret,
-            'grant_type' => $grantType
+            'appid'      => $this->appId,
+            'secret'     => $this->appSecret,
+            'grant_type' => $grantType,
         ]);
+
         return isset($result['access_token']) ? $result : false;
     }
 
@@ -143,14 +148,25 @@ class MiniProgram extends BaseWechat
     public function getJscode2Sessio($code)
     {
         $result = $this->httpGet(self::JSCODE_2_SESSSION_PATH, [
-            'appid' => $this->appId,
-            'secret' => $this->appSecret,
-            'js_code' => $code,
+            'appid'      => $this->appId,
+            'secret'     => $this->appSecret,
+            'js_code'    => $code,
             'grant_type' => 'authorization_code',
         ]);
 
         return !array_key_exists('errcode', $result) ? $result : false;
     }
+
+    public function getJscode2Mobile($code, $openid)
+    {
+        $result = $this->httpRaw(self::WXACODE_GETUSERPHONENUMBER, [
+            'openid' => $openid,
+            'code'   => $code,
+        ], ['access_token' => $this->getAccessToken()]);
+
+        return !array_key_exists('errcode', $result) ? $result : false;
+    }
+
 
     /**
      * 获取小程序二维码
@@ -204,28 +220,30 @@ class MiniProgram extends BaseWechat
     {
         if (empty($params['session_key'])) {
             $this->lastError = ['errcode' => -102, 'errmsg' => 'session Key不能为空'];
+
             return false;
         }
         //验证签名
         if (!empty($params['rawData']) && !empty($params['signature'])) {
-            $sign = sha1($params['rawData'] . $params['session_key']);
+            $sign = sha1($params['rawData'].$params['session_key']);
             if ($sign !== $params['signature']) {
                 $this->lastError = ['errcode' => -102, 'errmsg' => '签名不匹配'];
+
                 return false;
             }
         }
 
         // 使用sessionKey解密加密数据包
-        $pc = new WXBizDataCrypt($this->appId, $params['session_key']);
+        $pc      = new WXBizDataCrypt($this->appId, $params['session_key']);
         $errCode = $pc->decryptData($params['encryptedData'], $params['iv'], $data);
         if (!empty($errCode)) {
-            $this->lastError = ['errcode' => -103, 'errmsg' => '解密失败，错误码:'. $errCode];
+            $this->lastError = ['errcode' => -103, 'errmsg' => '解密失败，错误码:'.$errCode];
+
             return false;
         }
 
-        return  json_decode($data, true);
+        return json_decode($data, true);
     }
-
 
 
 }
